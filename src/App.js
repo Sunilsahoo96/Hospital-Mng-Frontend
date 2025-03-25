77e0d9cc9becd39a870d2d8e3735f89bb82c24d4
Sunilsahoo96 <sunilsahoo5075@gmail.com>
2025-03-25 11:48:21 +0530

    Changes made in app.js component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import "./Hospital.css";
 import AddMedicine from './components/AddMedicine';
-<<<<<<< HEAD
 import PatientRegistration from "./components/RegestrationPatient";
 import Auth from "./components/Auth";
 import Home from "./components/Home";
@@ -27,29 +26,7 @@ function App() {
         </div>
       </Router >
     </ThemeProviderWrapper>
-=======
-import PatientRegistration from "./components/Patient/RegestrationPatient";
-import Auth from "./components/Auth"; 
-import Home from "./components/Home"; 
-import AllMedicineDetails from "./components/AllMedicineDetails";
-import AllPatients from "./components/Patient/AllPatients";
-
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} /> 
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/add-medicine" element={<AddMedicine />} />
-          <Route path="/get-medicine" element={<AllMedicineDetails />} />
-          <Route path="/patient-registration" element={<PatientRegistration />} />
-          <Route path="/all-patient" element={<AllPatients/>} />
-        </Routes>
-      </div>
-    </Router>
->>>>>>> 5775d86dcaa6a9bfcf30406f06745694d672f610
-  );
+  )
 }
 
-export default App;
+export default App
